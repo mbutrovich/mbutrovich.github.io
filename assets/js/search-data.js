fcd80131c2b752b8d55e6266aab7e2657ff225dc
mbutrovich <mbutrovich@users.noreply.github.com>
2025-02-07 19:10:33 +0000

    Deploying to gh-pages from @ mbutrovich/mbutrovich.github.io@81462cfaf03e75dda9bfdcb55e3b17a9b268b78e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,13 +45,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/mbutrovich", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
